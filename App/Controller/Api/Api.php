2e2a0a7bd45b02a55c030acc7305f65c2584bc6a
Guilherme Recalcatte Vogel <guilhermerecalcatte@gmail.com>
2025-03-26 11:30:14 -0300

    corrigido null

--- App/Controller/Api/Api.php
+++ App/Controller/Api/Api.php
@@ -154,7 +154,7 @@ class Api
             $protocolo = $postVars['protocolo'] ?? throw new Exception('Protocolo não definido');
             $numero = $postVars['numero'] ?? throw new Exception('Número não definido');
             $codsercli = $postVars['codsercli'] ?? throw new Exception('Número não definido');
-            $id_massiva = $postVars['id_massiva'] ?? null;
+            $id_massiva = $postVars['id_massiva'] == '' ? null : $postVars['id_massiva'];
 
             $obMassiva = EntityMassiva::getMassivaByNumber($numero);
 
